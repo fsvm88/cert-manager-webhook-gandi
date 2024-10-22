a533f1641f3304569624ae9c9278cbc2c013d449
Fabio Scaccabarozzi <fsvm88@gmail.com>
2024-10-23 00:16:31 +0200

    minimal reformatting

--- main_test.go
+++ main_test.go
@@ -4,18 +4,15 @@ import (
 	"os"
 	"testing"
 
-	"github.com/cert-manager/cert-manager/test/acme"
+	dns "github.com/cert-manager/cert-manager/test/acme"
 )
 
-var (
-	zone = os.Getenv("TEST_ZONE_NAME")
-)
+var zone = os.Getenv("TEST_ZONE_NAME")
 
 func TestRunsSuite(t *testing.T) {
 	// The manifest path should contain a file named config.json that is a
 	// snippet of valid configuration that should be included on the
 	// ChallengeRequest passed as part of the test cases.
-
 	solver := &gandiDNSProviderSolver{}
 	fixture := dns.NewFixture(solver,
 		dns.SetResolvedZone(zone),
